35a0691591401ae411e14155356f6ecbf8b7dfe2
Raquel Leão <vc.raquel@gmail.com>
2021-11-18 10:18:40 -0300

    primeiro commit

---
+++ src/main/java/br/com/zup/gerenciador_lead_black_friday/GerenciadorLeadBlackFridayApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.gerenciador_lead_black_friday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorLeadBlackFridayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorLeadBlackFridayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/gerenciador_lead_black_friday/GerenciadorLeadBlackFridayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.gerenciador_lead_black_friday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorLeadBlackFridayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
